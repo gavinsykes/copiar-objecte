c5d6df3e1ff3b59a5b4fcf6681837c27a4352844
Gavin Sykes <gavin@gavinsykes.uk>
2020-07-13 11:40:07 +0100

    Adding recursion for when object's value is itself an object.

--- dev/src/copyObject.ts
+++ dev/src/copyObject.ts
@@ -1,6 +1,10 @@
+const isObject: (item: any) => boolean = function(item) {
+  return item && typeof item === "object" && !Array.isArray(item);
+}
+
 function copyObject(original: any): object {
   let returnedObject = {};
-  Object.entries(original).map((c,i,a) => returnedObject[c[0]] = c[1]);
+  Object.entries(original).map(c => returnedObject[c[0]] = isObject(c[1]) ? copyObject(c[1]) : c[1]);
   return returnedObject;
 }
 
